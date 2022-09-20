338c9468efc387b42b71bf0e389f9c31406a1ddc
Robert Kehoe <113054199+rkehoejr11@users.noreply.github.com>
2022-09-19 18:48:36 -0600

    My person version of "Hello World"...
    First ever publicly available py script, here goes nothing!

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# A Built-In Hello World Module
+import __hello__
+
+# The Zen of Python
+import this
+
+# Entertainment Built Right In
+import antigravity
+
+
+def hello_world():
+    """This will be my first publicly available python script uploaded to GitHub, here's to what will hopefully be
+    a long career in software!!"""
+
+    print()
+    print('Hi, my name is Rob. I personally love all the cool Easter Eggs that can be found in Python :-)!!')
+
+
+if __name__ == '__main__':
+    hello_world()
